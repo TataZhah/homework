f89b9cdc932dd99fa69e87c0d7c817fb17aeb203
Artsiom Fiadotau <zhahlivy@gmail.com>
2020-02-24 04:39:25 +0300

    result

---
+++ Task3/src/FindSeven.java
@@ -0,0 +1,12 @@
+
+public class FindSeven {
+
+    public static void main(String[] args) {
+
+        int x = 27;
+        if (x % 10 == 7)
+        System.out.println("На конце числа - 7");
+        else
+        System.out.println("Нет");
+    }
+}
